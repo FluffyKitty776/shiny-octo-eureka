9b7e3237603ddee30bd85023efa184f1d01819ad
KitKat Cat <KitKat-Cat@users.noreply.github.com>
2017-12-02 23:19:19 -0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+IncludeHTML = function(url, cb) {
+  var z, i, elmnt, file, xhttp;
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    file = elmnt.getAttribute("include");;
+    if (file) {
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (this.readyState == 4 && this.status == 200) {
+          elmnt.innerHTML = this.responseText;
+          
+          w3.includeHTML(cb);
+        }
+      }      
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      return;
+    }
+  }
+  if (cb) cb();
+};
+IncludeHTML();
+
+var myFunction = function(url) {
+ document.getElementById("target").include=url;
+}
